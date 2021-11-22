051fa6941fedc132bfd13ae88053d6e15bd83e41
Adán Fernández Sánchez <UO251162@uniovi.es>
2021-11-22 18:17:10 +0100

    Ruta relativa

--- Navegador/src/Main.java
+++ Navegador/src/Main.java
@@ -9,9 +9,9 @@ import simpleHtml.visitor.PrintVisitor;
 
 public class Main {
 	static boolean logGen = true;
-	
 	public static void main(String[] args) throws FileNotFoundException {
-		FileReader fileReader = new FileReader("C:\\Users\\adanf\\Desktop\\aquilino\\Software-Architectures-and-Language-Processing\\Navegador\\src\\EX4.html");
+		//FileReader fileReader = new FileReader("C:\\Users\\uo251162\\Documents\\Software-Architectures-and-Language-Processing\\Navegador\\src\\EX4.html");
+		FileReader fileReader = new FileReader("src\\EX4.html");
 		Lexicon lexicon = new Lexicon(fileReader);
 		Parser parser = new Parser(lexicon);
 		Programa programa = parser.parse();
